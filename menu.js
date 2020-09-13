b733d2bb4ef0a628e668ef58c9d5660cd56b9c67
nagaha1 <nagaha1221@email.com>
2020-09-13 16:12:25 +0900

    tes

--- menu.js
+++ menu.js
@@ -1,6 +1,16 @@
-
 // Drawerを有効化させます。
 $(document).ready(function() {
-    $('.drawer').drawer();
+  $(".drawer").drawer();
+});
+
+//ripples背景画像を波立たせる
+$(function() {
+  let $hoge = $(".header-bg");
+  $hoge.ripples({
+    dropRadius: 20,
+    perturbance: 0.03,
+    resolution: 256,
+    interactive: true,
+    crossOrigin: ""
   });
-  
+});
